99286ad502a93adca2efc5da7de6ffe5e486494f
Yuto Uchimi <momosama1072@gmail.com>
2017-11-09 17:52:18 +0900

    Initial completion of detect plane and put mesh

--- src/plane_detection.cpp
+++ src/plane_detection.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <random>
 #include <string>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <math.h>
 #include <opencv2/opencv.hpp>
 
-#include <pcl/ModelCoefficients.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/filters/project_inliers.h>
-#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/common/transforms.h>
+#include <pcl/features/normal_3d.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 #include <yaml-cpp/yaml.h>
 
@@ -44,11 +50,8 @@ void planeRemoval(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
 }
 
 
-int main(int argc, char** argv) {
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-  pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-
+int main(int argc, char** argv)
+{
   std::string data_path;
   if (argc == 2) {
     data_path = std::string(argv[1]);
@@ -69,6 +72,8 @@ int main(int argc, char** argv) {
     cam_K_[0], cam_K_[1], cam_K_[2],
     cam_K_[3], cam_K_[4], cam_K_[5],
     cam_K_[6], cam_K_[7], cam_K_[8];
+  std::cout << "width : " << width << std::endl;
+  std::cout << "height : " << height << std::endl;
   std::cout << "cam_K : " << std::endl << cam_K << std::endl;
 
   // std::string cam_pose_file = data_path + "tf_camera_rgb_from_base.yaml";
@@ -97,14 +102,10 @@ int main(int argc, char** argv) {
     r10, r11, r12, ty,
     r20, r21, r22, tz,
     0,   0,   0,   1;
-  std::cout << "cam_pose : " << std::endl << cam_pose << std::endl;
+  std::cout << "cam_pose : " << std::endl << cam_pose << std::endl << std::endl;
 
   std::string depth_file = data_path + "depth_obj_n.png";
-
-
-
   cv::Mat depth_raw = cv::imread(depth_file, CV_LOAD_IMAGE_UNCHANGED);
-
   cv::Mat depth(depth_raw.rows, depth_raw.cols, CV_32FC1);
   for (size_t j = 0; j < depth_raw.rows; ++j)
   {
@@ -122,6 +123,10 @@ int main(int argc, char** argv) {
     }
   }
 
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr original_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  original_cloud->width = width;
+  original_cloud->height = height;
+
   for (int v = 0; v < height; v++) {
     for (int u = 0; u < width; u++) {
       double d = std::numeric_limits<float>::quiet_NaN();
@@ -135,27 +140,216 @@ int main(int argc, char** argv) {
         direction_(1) *= d;
         direction_(2) = d;
       }
+      else {
+        direction_(0) = std::numeric_limits<float>::quiet_NaN();
+        direction_(1) = std::numeric_limits<float>::quiet_NaN();
+        direction_(2) = std::numeric_limits<float>::quiet_NaN();
+      }
 
       direction_ = cam_pose * direction_;
       Eigen::Vector3f direction(direction_(0), direction_(1), direction_(2));
 
-      pcl::PointXYZRGB pt(255, 255, 255);
+      pcl::PointXYZRGB pt(255, 255, 0);
       pt.x = direction(0);
       pt.y = direction(1);
       pt.z = direction(2);
-      cloud->points.push_back(pt);
+      original_cloud->points.push_back(pt);
     }
   }
-  pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-  viewer.showCloud (cloud);
-  while (!viewer.wasStopped ()) {}
 
-  planeDetect(cloud, inliers, coefficients, 0.01);
-  planeRemoval(cloud, inliers, false);
+  std::cout << "original_cloud->points.size() : " << original_cloud->points.size() << std::endl;
 
-  // pcl::PCDWriter writer;
-  // writer.write("cloud_out.pcd", *cloud, false);
-  cloud->clear();
+  // Plane extraction
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::copyPointCloud(*original_cloud, *cloud_filtered);
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+  planeDetect(cloud_filtered, inliers, coefficients, 0.02);
+  std::cout << "inliers->indices.size() : " << inliers->indices.size() << std::endl;
+  planeRemoval(cloud_filtered, inliers, false);
+
+  // Creating the KdTree object for the search method of the extraction
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_mono(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::copyPointCloud(*cloud_filtered, *cloud_filtered_mono);
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  tree->setInputCloud (cloud_filtered_mono);
+
+  // Euclidean Clustering
+  std::vector<pcl::PointIndices> cluster_indices;
+  pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+  ec.setClusterTolerance (0.02); // 2cm
+  ec.setMinClusterSize (100);
+  ec.setMaxClusterSize (height * width);
+  ec.setSearchMethod (tree);
+  ec.setInputCloud (cloud_filtered_mono);
+  ec.extract (cluster_indices);
+  std::cout << "cluster_indices.size() : " << cluster_indices.size() << std::endl;
+  for (int i = 0; i < cluster_indices.size() ; i++)
+  {
+    std::cout << "cluster_indices[" << i << "].indices.size() : " << cluster_indices[i].indices.size() << std::endl;
+  }
+  std::cout << std::endl;
+
+  // Choose maximum size from clusters
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+  for (std::vector<int>::const_iterator pit = cluster_indices[0].indices.begin(); pit != cluster_indices[0].indices.end(); ++pit)
+  {
+    cloud_cluster->points.push_back(cloud_filtered_mono->points[*pit]);
+  }
+  cloud_cluster->width = cloud_cluster->points.size ();
+  cloud_cluster->height = 1;
+  cloud_cluster->is_dense = true;
+
+  // Compute max_x_pln, max_y_pln, ... , min_z_pln of euclidean-clustered-max-cloud
+  float max_x_pln, max_y_pln, max_z_pln, min_x_pln, min_y_pln, min_z_pln;
+  float tmp_max_x_pln, tmp_max_y_pln, tmp_max_z_pln, tmp_min_x_pln, tmp_min_y_pln, tmp_min_z_pln;
+  tmp_max_x_pln = tmp_max_y_pln = tmp_max_z_pln = -1000;
+  tmp_min_x_pln = tmp_min_y_pln = tmp_min_z_pln = 1000;
+  for (pcl::PointCloud<pcl::PointXYZ>::iterator it = cloud_cluster->points.begin(); it != cloud_cluster->points.end(); ++it)
+  {
+    max_x_pln = std::max(it->x, tmp_max_x_pln);
+    max_y_pln = std::max(it->y, tmp_max_y_pln);
+    max_z_pln = std::max(it->z, tmp_max_z_pln);
+    min_x_pln = std::min(it->x, tmp_min_x_pln);
+    min_y_pln = std::min(it->y, tmp_min_y_pln);
+    min_z_pln = std::min(it->z, tmp_min_z_pln);
+    tmp_max_x_pln = max_x_pln;
+    tmp_max_y_pln = max_y_pln;
+    tmp_max_z_pln = max_z_pln;
+    tmp_min_x_pln = min_x_pln;
+    tmp_min_y_pln = min_y_pln;
+    tmp_min_z_pln = min_z_pln;
+  }
+
+  // Compute centroid
+  Eigen::Vector4f pln_centroid;
+  pcl::compute3DCentroid(*cloud_cluster, pln_centroid);
+
+  std::cout << "cloud_cluster :" << std::endl;
+  std::cout << "  - centroid : (" << pln_centroid[0] << ", " << pln_centroid[1] << ", " << pln_centroid[2] << ")" << std::endl;
+  std::cout << "  - x range : " << min_x_pln << " ~ " << max_x_pln << std::endl;
+  std::cout << "  - y range : " << min_y_pln << " ~ " << max_y_pln << std::endl;
+  std::cout << "  - z range : " << min_z_pln << " ~ " << max_z_pln << std::endl << std::endl;
+
+  // Load mesh model
+  std::string mesh_file = data_path + "data/mvtk/transparent_objects/ShapeNetCore.v2.scaled/02876657/f49d6c4b75f695c44d34bdc365023cf4/models/model_normalized.obj";
+  pcl::PolygonMesh::Ptr mesh(new pcl::PolygonMesh());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr obj_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+  if (pcl::io::loadPolygonFileOBJ(mesh_file, *mesh) != -1)
+  {
+    // PolygonMesh -> PointCloud<PointXYZ>
+    pcl::fromPCLPointCloud2(mesh->cloud, *obj_cloud);
+  }
+
+  // Rotate obj_cloud at random angles
+  std::uniform_int_distribution<int> mesh_rot_x_(0, 3);
+  std::uniform_int_distribution<int> mesh_rot_y_(0, 3);
+  std::uniform_real_distribution<float> mesh_rot_z_(0, 2 * M_PI);
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  float mesh_rot_x = mesh_rot_x_(mt) * M_PI / 2;
+  float mesh_rot_y = mesh_rot_y_(mt) * M_PI / 2;
+  float mesh_rot_z = mesh_rot_z_(mt);
+  Eigen::Matrix4f R_x, R_y, R_z, R;
+  R_x <<
+    1, 0,               0,                0,
+    0, cos(mesh_rot_x), -sin(mesh_rot_x), 0,
+    0, sin(mesh_rot_x), cos(mesh_rot_x),  0,
+    0, 0,               0,                1;
+  R_y <<
+    cos(mesh_rot_y),  0, sin(mesh_rot_y), 0,
+    0,                1, 0,               0,
+    -sin(mesh_rot_y), 0, cos(mesh_rot_y), 0,
+    0,                0, 0,               1;
+  R_z <<
+    cos(mesh_rot_z), -sin(mesh_rot_z), 0, 0,
+    sin(mesh_rot_z), cos(mesh_rot_z),  0, 0,
+    0,               0,                1, 0,
+    0,               0,                0, 1;
+  R = R_z * R_y * R_x;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr rotated_obj_cloud (new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::transformPointCloud(*obj_cloud, *rotated_obj_cloud, R);
+
+  // Compute max_x_obj, max_y_obj, ... , min_z_obj of rotated_obj_cloud
+  float max_x_obj, max_y_obj, max_z_obj, min_x_obj, min_y_obj, min_z_obj;
+  float tmp_max_x_obj, tmp_max_y_obj, tmp_max_z_obj, tmp_min_x_obj, tmp_min_y_obj, tmp_min_z_obj;
+  tmp_max_x_obj = tmp_max_y_obj = tmp_max_z_obj = -1000;
+  tmp_min_x_obj = tmp_min_y_obj = tmp_min_z_obj = 1000;
+  for (pcl::PointCloud<pcl::PointXYZ>::iterator it = rotated_obj_cloud->points.begin(); it != rotated_obj_cloud->points.end(); ++it)
+  {
+    max_x_obj = std::max(it->x, tmp_max_x_obj);
+    max_y_obj = std::max(it->y, tmp_max_y_obj);
+    max_z_obj = std::max(it->z, tmp_max_z_obj);
+    min_x_obj = std::min(it->x, tmp_min_x_obj);
+    min_y_obj = std::min(it->y, tmp_min_y_obj);
+    min_z_obj = std::min(it->z, tmp_min_z_obj);
+    tmp_max_x_obj = max_x_obj;
+    tmp_max_y_obj = max_y_obj;
+    tmp_max_z_obj = max_z_obj;
+    tmp_min_x_obj = min_x_obj;
+    tmp_min_y_obj = min_y_obj;
+    tmp_min_z_obj = min_z_obj;
+  }
+
+  // Move rotated_obj_cloud onto the plane
+  float max_x = max_x_pln - max_x_obj;
+  float max_y = max_y_pln - max_y_obj;
+  float min_x = min_x_pln - min_x_obj;
+  float min_y = min_y_pln - min_y_obj;
+  std::uniform_real_distribution<float> mesh_orig_x_(min_x, max_x);
+  std::uniform_real_distribution<float> mesh_orig_y_(min_y, max_y);
+  float mesh_orig_x = mesh_orig_x_(mt);
+  float mesh_orig_y = mesh_orig_y_(mt);
+  float mesh_orig_z = pln_centroid[2] - min_z_obj;
+  Eigen::Matrix4f transform = R;
+  transform(0, 3) = mesh_orig_x;
+  transform(1, 3) = mesh_orig_y;
+  transform(2, 3) = mesh_orig_z;
+  std::cout << "transform :" << std::endl << transform << std::endl << std::endl;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr obj_cloud_on_plane(new pcl::PointCloud<pcl::PointXYZRGB>);
+  for (pcl::PointCloud<pcl::PointXYZ>::iterator it = rotated_obj_cloud->points.begin(); it != rotated_obj_cloud->points.end(); ++it)
+  {
+    pcl::PointXYZRGB pt(255, 0, 0);
+    pt.x = mesh_orig_x + it->x;
+    pt.y = mesh_orig_y + it->y;
+    pt.z = mesh_orig_z + it->z;
+    obj_cloud_on_plane->points.push_back(pt);
+  }
+  for (pcl::PointCloud<pcl::PointXYZ>::iterator it = cloud_cluster->points.begin(); it != cloud_cluster->points.end(); ++it)
+  {
+    pcl::PointXYZRGB pt(255, 255, 255);
+    pt.x = it->x;
+    pt.y = it->y;
+    pt.z = it->z;
+    obj_cloud_on_plane->points.push_back(pt);
+  }
+
+  // View PointCloud
+  // pcl::visualization::CloudViewer viewer0("original_cloud");
+  // viewer0.showCloud (original_cloud);
+  // while (!viewer0.wasStopped()) {}
+  // pcl::visualization::CloudViewer viewer1("cloud_cluster");
+  // viewer1.showCloud (cloud_cluster);
+  // while (!viewer1.wasStopped()) {}
+  // pcl::visualization::CloudViewer viewer2("obj_cloud");
+  // viewer2.showCloud (obj_cloud);
+  // while (!viewer2.wasStopped()) {}
+  pcl::visualization::CloudViewer viewer3("obj_cloud_on_plane");
+  viewer3.showCloud (obj_cloud_on_plane);
+  while (!viewer3.wasStopped()) {}
+
+  // Save as .pcd file
+  std::cout << "cloud_cluster->points.size() : " << cloud_cluster->points.size() << std::endl;
+  std::string out_file = data_path + "cloud_obj_n.pcd";
+  pcl::io::savePCDFile(out_file, *cloud_cluster);
+  std::cout << "Wrote point cloud to: " << out_file << std::endl;
+
+  original_cloud->clear();
+  cloud_filtered->clear();
+  cloud_filtered_mono->clear();
+  cloud_cluster->clear();
+  obj_cloud->clear();
+  obj_cloud_on_plane->clear();
 
   return (0);
 }
