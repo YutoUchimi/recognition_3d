a2b1d091f6414a5bfdf10c3561d12935ea3e29ef
Yuto Uchimi <momosama1072@gmail.com>
2018-02-01 13:44:22 +0900

    Add models for pr2 refrigerator demo

--- node_scripts/models/__init__.py
+++ node_scripts/models/__init__.py
@@ -1,4 +1,7 @@
 # flake8: noqa
 from fcn32s import FCN32s
 from fcn8s import FCN8sAtOnce
+from fcn8s import FCN8sAtOnceConcatAtOnce
 from fcn8s import FCN8sAtOnceInputRGBD
+from fcn8s import FCN8sAtOnceOnlyDepth
+from fcn8s import FCN8sAtOnceOnlyLabel
